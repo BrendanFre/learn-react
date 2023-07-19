18dc0db8a8a462ff1b9c72d9c17a387db93d7f48
Brendan Freeman <bfreeman0@gmail.com>
2023-07-19 19:38:34 +0100

    form create

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
+  const name = "Brad";
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Header />
+      <h1>Hello from React</h1>
+      <h2>This is a H2</h2>
+      <h3>Hi {name}</h3>
+    </div>
   )
 }
 
